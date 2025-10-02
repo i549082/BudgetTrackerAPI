1c23b33e31d8440b488227edab2260c72ceac98b
ak13 <i549082@fontysict.nl>
2025-10-02 19:20:42 +0200

    test fix

--- src/test/java/com/BudgetTracker/BudgetTrackerAPI/BudgetTrackerApiApplicationTests.java
+++ src/test/java/com/BudgetTracker/BudgetTrackerAPI/BudgetTrackerApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.BudgetTracker.BudgetTrackerAPI;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class BudgetTrackerApiApplicationTests {
 
 	@Test
