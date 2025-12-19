1a5363a23310c76fbd6269a725fbb0175b9a040d
ak13 <i549082@fontysict.nl>
2025-12-19 10:23:15 +0100

    flyway check

--- src/main/java/com/BudgetTracker/BudgetTrackerAPI/DataAccess/Entities/PersonEntity.java
+++ src/main/java/com/BudgetTracker/BudgetTrackerAPI/DataAccess/Entities/PersonEntity.java
@@ -21,7 +21,7 @@ public class PersonEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+///
     @Column(name = "username", nullable = false, unique = true, length = 50)
     private String username;
 
