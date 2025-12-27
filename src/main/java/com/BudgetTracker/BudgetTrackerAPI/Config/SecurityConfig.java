85eab1945d95f6c728dfffc50dfb220c09a355e1
ak13 <i549082@fontysict.nl>
2025-12-27 02:54:41 +0200

    cors issue fix

--- src/main/java/com/BudgetTracker/BudgetTrackerAPI/Config/SecurityConfig.java
+++ src/main/java/com/BudgetTracker/BudgetTrackerAPI/Config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,6 +36,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+
 
                 .authorizeHttpRequests(
                         req -> req.requestMatchers("/api/login",
@@ -45,7 +48,8 @@ public class SecurityConfig {
                                         "/v3/api-docs/**",
                                         "/swagger-resources/**",
                                         "/webjars/**",
-                                        "/v2/api-docs/**")
+                                        "/v2/api-docs/**"
+                                        )
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
--- src/main/java/com/BudgetTracker/BudgetTrackerAPI/Controllers/AuthenticationController.java
+++ src/main/java/com/BudgetTracker/BudgetTrackerAPI/Controllers/AuthenticationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
